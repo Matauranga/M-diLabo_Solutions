d897dd8510120160f2edfa33b8d10fa0f4174e93
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2023-12-08 00:19:58 +0100

    S1-Frontend : adding PatientBean

---
+++ frontend-management/src/main/java/com/mediLaboSolutions/frontendmanagement/beans/PatientBean.java
@@ -0,0 +1,32 @@
+package com.mediLaboSolutions.frontendmanagement.beans;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PatientBean {
+
+    private String firstname;
+    private String lastname;
+    private LocalDate birthdate;
+    private String gender;
+    private String address;
+    private String phonenumber;
+
+    @Override
+    public String toString() {
+        return "PatientBean{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", birthdate=" + birthdate +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                ", phonenumber='" + phonenumber + '\'' +
+                '}';
+    }
+}
