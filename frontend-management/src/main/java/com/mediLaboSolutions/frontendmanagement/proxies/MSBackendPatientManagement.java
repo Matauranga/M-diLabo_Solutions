d19396c60d960f9e4412ebf3c1422569978e05e8
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2023-12-08 00:19:24 +0100

    S1-Frontend : adding proxy interface

---
+++ frontend-management/src/main/java/com/mediLaboSolutions/frontendmanagement/proxies/MSBackendPatientManagement.java
@@ -0,0 +1,19 @@
+package com.mediLaboSolutions.frontendmanagement.proxies;
+
+import com.mediLaboSolutions.frontendmanagement.beans.PatientBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "patient", url = "localhost:9001")
+public interface MSBackendPatientManagement {
+
+    @GetMapping(value = "/patients")
+    List<PatientBean> patientsList();
+
+    @GetMapping(value = "/patient")
+    PatientBean patientInfos(@RequestParam String lastname,
+                             @RequestParam String firstname);
+}
