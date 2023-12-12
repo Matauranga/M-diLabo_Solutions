6ff6174a9632deefc8d5203f9170ee78b4106e9f
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2023-12-12 23:43:35 +0100

    S1-Gateway : arrangement to use gateway

--- frontend-management/src/main/java/com/mediLaboSolutions/frontendmanagement/proxies/MSBackendPatientManagement.java
+++ frontend-management/src/main/java/com/mediLaboSolutions/frontendmanagement/proxies/MSBackendPatientManagement.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "MS-BACKEND-PATIENT-MANAGEMENT")
+@FeignClient(name = "MS-GATEWAY")
 public interface MSBackendPatientManagement {
 
     @GetMapping(value = "/patients")
