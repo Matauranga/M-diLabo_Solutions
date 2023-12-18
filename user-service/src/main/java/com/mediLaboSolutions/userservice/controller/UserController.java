147dfe4a946d69df29cc2dc6096504eeb2a39bff
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2023-12-18 15:07:35 +0100

    S1-User-service : update user service

---
+++ user-service/src/main/java/com/mediLaboSolutions/userservice/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.mediLaboSolutions.userservice.controller;
+
+import com.mediLaboSolutions.userservice.services.UserService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}
--- user-service/src/main/java/com/mediLaboSolutions/userservice/services/UserServiceImpl.java
+++ user-service/src/main/java/com/mediLaboSolutions/userservice/services/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.mediLaboSolutions.userservice.services;
 import com.mediLaboSolutions.userservice.exceptions.UserNotFoundException;
 import com.mediLaboSolutions.userservice.models.Users;
 import com.mediLaboSolutions.userservice.repositories.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
