bd035d6e1eb1ee1275313f1010c47067e3d8f9a1
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-02 11:44:05 +0100

    S1-FRONTEND : change home page to "/patients" from "/"

--- frontend-management/src/main/java/com/mediLaboSolutions/frontendmanagement/controller/FrontController.java
+++ frontend-management/src/main/java/com/mediLaboSolutions/frontendmanagement/controller/FrontController.java
@@ -1,9 +1,11 @@
 package com.mediLaboSolutions.frontendmanagement.controller;
 
+import com.mediLaboSolutions.frontendmanagement.DTO.AuthRequest;
 import com.mediLaboSolutions.frontendmanagement.beans.NewPatientBean;
 import com.mediLaboSolutions.frontendmanagement.beans.PatientBean;
 import com.mediLaboSolutions.frontendmanagement.beans.PatientToUpdateBean;
-import com.mediLaboSolutions.frontendmanagement.proxies.MSBackendPatientManagement;
+import com.mediLaboSolutions.frontendmanagement.proxies.AuthService;
+import com.mediLaboSolutions.frontendmanagement.proxies.MSGateWay;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -20,14 +22,15 @@ public class FrontController {
 
     private final MSBackendPatientManagement msBackendPatientManagement;
 
-    public FrontController(MSBackendPatientManagement msBackendPatientManagement) {
-        this.msBackendPatientManagement = msBackendPatientManagement;
+    public FrontController(MSGateWay msGateWay, AuthService authService) {
+        this.msGateWay = msGateWay;
+        this.authService = authService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/patients")
     public String home(Model model) {
 
-        List<PatientBean> patients = msBackendPatientManagement.patientsList();
+        List<PatientBean> patients = msGateWay.patientsList();
         model.addAttribute("patients", patients);
 
         return "patients";
@@ -36,7 +39,7 @@ public class FrontController {
     @GetMapping("/patients/{id}")
     public String patientInfos(@PathVariable Integer id, Model model) {
 
-        final PatientBean patient = msBackendPatientManagement.patientInfos(id);
+        final PatientBean patient = msGateWay.patientInfos(id);
         model.addAttribute("patient", patient);
 
         return "patient-details";
@@ -53,7 +56,7 @@ public class FrontController {
     @PostMapping("/patients/create")
     public String createNewPatient(@Valid NewPatientBean newPatientBean, Model model) {
 
-        msBackendPatientManagement.createPatient(newPatientBean);
+        msGateWay.createPatient(newPatientBean);
         log.info("Front --> Ask to create patient : {} + {}", newPatientBean.getFirstname(), newPatientBean.getLastname());
 
         return home(model);
@@ -63,10 +66,30 @@ public class FrontController {
     public String editPatient(@PathVariable Integer id, @Valid PatientToUpdateBean patientToUpdateBean, Model model) {
 
         log.info("Front --> Ask to update patient : {} + {}", patientToUpdateBean.getFirstname(), patientToUpdateBean.getLastname());
-        final PatientToUpdateBean updatedPatient = msBackendPatientManagement.updatePatient(String.valueOf(id), patientToUpdateBean);
+        final PatientToUpdateBean updatedPatient = msGateWay.updatePatient(String.valueOf(id), patientToUpdateBean);
         model.addAttribute("patient", updatedPatient);
 
         return patientInfos(id, model);
+    }
+
+    @GetMapping({"/", "/signin"})
+    public String login(Model model) {
+        model.addAttribute("authRequest", new AuthRequest());
+
+        return "login";
+    }
+
+    @PostMapping("/signin")
+    public String signin(AuthRequest authRequest, Model model) {
+        try {
+            final String token = msGateWay.login(authRequest);
+            authService.saveToken(token);
+
+            return home(model);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return login(model); //todo gerer err
+        }
     }
 
 }
