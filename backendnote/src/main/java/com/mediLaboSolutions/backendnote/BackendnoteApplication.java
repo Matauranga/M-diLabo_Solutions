3fb37fc38e059a958e28ebca67e36f60b1f1b539
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-05 19:12:44 +0100

    S2-BACKEND-NOTE : add microservice backend note

---
+++ backendnote/src/main/java/com/mediLaboSolutions/backendnote/BackendnoteApplication.java
@@ -0,0 +1,13 @@
+package com.mediLaboSolutions.backendnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendnoteApplication.class, args);
+	}
+
+}
---
+++ backendnote/src/test/java/com/mediLaboSolutions/backendnote/BackendnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediLaboSolutions.backendnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
