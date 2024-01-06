f6f5154aaf915cfb59b76fc069448620fd2074cb
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-06 01:11:17 +0100

    S2-BACKEND-NOTE : arrangement

--- backend-note/src/main/java/com/mediLaboSolutions/backendnote/BackendNoteApplication.java
+++ backend-note/src/main/java/com/mediLaboSolutions/backendnote/BackendNoteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendnoteApplication {
+public class BackendNoteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendnoteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendNoteApplication.class, args);
+    }
 
 }
