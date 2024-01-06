9c9479b7571543270590be55f7cca4c9c6c3fcdc
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-06 16:35:34 +0100

    S2-BACKEND-NOTE : update of note controller

--- backend-note/src/main/java/com/mediLaboSolutions/backendnote/controller/NoteController.java
+++ backend-note/src/main/java/com/mediLaboSolutions/backendnote/controller/NoteController.java
@@ -3,51 +3,42 @@ package com.mediLaboSolutions.backendnote.controller;
 
 import com.mediLaboSolutions.backendnote.DTO.NoteDTO;
 import com.mediLaboSolutions.backendnote.models.Note;
-import com.mediLaboSolutions.backendnote.repositories.NoteRepository;
+import com.mediLaboSolutions.backendnote.services.NoteService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RestController
 public class NoteController { //TODO revoir path avec Frank
 
-    @Autowired
-    NoteRepository noteRepository;
+    final NoteService noteService;
 
-    @GetMapping("/notes/{patientId}/{id}")
-    public Optional<Note> getNote(@PathVariable String patientId, @PathVariable String id) {
-
-        Optional<Note> note = noteRepository.findById(id);
-
-        return note;
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
     }
+
+//    @GetMapping("/notes/{patientId}/{id}")//TODO frank
+//    public Note getNote(@PathVariable String patientId, @PathVariable String id) {
+//
+//        return noteService.getOneNote(id);
+//    }
 
     @GetMapping("/notes/{patientId}")
     public List<Note> getAllPatientNotes(@PathVariable String patientId) {
 
-        List<Note> notes = noteRepository.findByPatientIdOrderByDateDesc(patientId);
-
-        return notes;
+        return noteService.getAllPatientNotes(patientId);
     }
 
     @PostMapping("/notes")
     public ResponseEntity<Note> addNote(@RequestBody NoteDTO noteDTO) {
 
-        Note noteToCreate = new Note();
-        noteToCreate.setPatientId(noteDTO.getPatientId());
-        noteToCreate.setContent(noteDTO.getContent());
-        noteToCreate.setDate(new Date());
+        // Note noteToCreate = noteService.createNewPatientNote(noteDTO); TODO Frank
 
-        noteRepository.insert(noteToCreate);
-
-        return new ResponseEntity<>(noteToCreate, HttpStatus.CREATED);
+        return new ResponseEntity<>(noteService.createNewPatientNote(noteDTO), HttpStatus.CREATED);
     }
 
 
