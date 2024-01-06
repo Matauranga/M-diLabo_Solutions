19d06ebf93a609de302fcf7a4f9035d85178e222
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-06 01:11:33 +0100

    S2-BACKEND-NOTE : add model and DTO for Note

---
+++ backend-note/src/main/java/com/mediLaboSolutions/backendnote/DTO/NoteDTO.java
@@ -0,0 +1,16 @@
+package com.mediLaboSolutions.backendnote.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoteDTO {
+
+    private String patientId;
+    private String content;
+}
---
+++ backend-note/src/main/java/com/mediLaboSolutions/backendnote/model/Note.java
@@ -0,0 +1,28 @@
+package com.mediLaboSolutions.backendnote.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "notes")
+public class Note {
+
+    @Id
+    private String id;
+
+    private String patientId;
+
+    private String content;
+
+    private Date date;
+
+}
