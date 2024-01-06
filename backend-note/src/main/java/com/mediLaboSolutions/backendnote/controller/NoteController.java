c6a64308d864f59af4e7b7be18cdb68a20815adb
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-06 01:12:01 +0100

    S2-BACKEND-NOTE : add controller

---
+++ backend-note/src/main/java/com/mediLaboSolutions/backendnote/controller/NoteController.java
@@ -0,0 +1,54 @@
+package com.mediLaboSolutions.backendnote.controller;
+
+
+import com.mediLaboSolutions.backendnote.DTO.NoteDTO;
+import com.mediLaboSolutions.backendnote.model.Note;
+import com.mediLaboSolutions.backendnote.repository.NoteRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RestController
+public class NoteController { //TODO revoir path avec Frank
+
+    @Autowired
+    NoteRepository noteRepository;
+
+    @GetMapping("/notes/{patientId}/{id}")
+    public Optional<Note> getNote(@PathVariable String patientId, @PathVariable String id) {
+
+        Optional<Note> note = noteRepository.findById(id);
+
+        return note;
+    }
+
+    @GetMapping("/notes/{patientId}")
+    public List<Note> getAllPatientNotes(@PathVariable String patientId) {
+
+        List<Note> note = noteRepository.findByPatientIdOrderByDateDesc(patientId);
+
+        return note;
+    }
+
+    @PostMapping("/notes")
+    public ResponseEntity<Note> addNote(@RequestBody NoteDTO noteDTO) {
+
+        Note noteToCreate = new Note();
+        noteToCreate.setPatientId(noteDTO.getPatientId());
+        noteToCreate.setContent(noteDTO.getContent());
+        noteToCreate.setDate(new Date());
+
+        noteRepository.insert(noteToCreate);
+
+        return new ResponseEntity<>(noteToCreate, HttpStatus.CREATED);
+    }
+
+
+}
