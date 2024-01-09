fa10644e3e9b393cb9d8348b42728c39963c8668
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-09 18:12:17 +0100

    S2-FRONTEND : add todo

--- frontend-management/src/test/java/com/mediLaboSolutions/frontendmanagement/controller/PatientControllerTest.java
+++ frontend-management/src/test/java/com/mediLaboSolutions/frontendmanagement/controller/PatientControllerTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class PatientControllerTest {
+class PatientControllerTest { //todo mock feign
 
     @Autowired
     private MockMvc mockMvc;
