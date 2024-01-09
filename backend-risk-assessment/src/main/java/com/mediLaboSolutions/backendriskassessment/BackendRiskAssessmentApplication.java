a320bdcc5043d8e601ac8f3215355cc43a6e087a
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-09 18:19:48 +0100

    S3-BACKEND-RISK : add module

---
+++ backend-risk-assessment/src/main/java/com/mediLaboSolutions/backendriskassessment/BackendRiskAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.mediLaboSolutions.backendriskassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRiskAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRiskAssessmentApplication.class, args);
+	}
+
+}
