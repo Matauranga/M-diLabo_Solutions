cf3109d60f0120bb0290bb7ae4340c9c415a2e59
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-10 19:26:38 +0100

    S3-BACKEND-RISK : add 2 constants --> Trigger terms and Result terms

---
+++ backend-risk-assessment/src/main/java/com/mediLaboSolutions/backendriskassessment/constants/AllTriggerTerms.java
@@ -0,0 +1,22 @@
+package com.mediLaboSolutions.backendriskassessment.constants;
+
+import java.util.List;
+
+public class AllTriggerTerms {
+    //TODO comprendre la liste des mots à fournir
+    public static final List<String> triggerTerms = List.of(
+
+            "Hémoglobine A1C",
+            "Microalbumine",
+            "Taille",
+            "Poids",
+            "Fume",
+            "Anormal",
+            "Cholestérol",
+            "Vertiges",
+            "Rechute",
+            "Réaction",
+            "Anticorps"
+
+    );
+}
---
+++ backend-risk-assessment/src/main/java/com/mediLaboSolutions/backendriskassessment/constants/TermsRiskAssessmentResults.java
@@ -0,0 +1,12 @@
+package com.mediLaboSolutions.backendriskassessment.constants;
+
+public class TermsRiskAssessmentResults {
+    public static final String NONE = "None";
+
+    public static final String BORDERLINE = "Borderline";
+
+    public static final String IN_DANGER = "InDanger";
+
+    public static final String EARLY_ONSET = "EarlyOnset";
+
+}
