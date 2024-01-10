83fc239bfb03b833ddb9ab10bee745b20052704f
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-10 01:58:37 +0100

    S3-BACKEND-RISK : add test file

---
+++ backend-risk-assessment/src/test/java/com/mediLaboSolutions/backendriskassessment/services/AssessmentRiskServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.mediLaboSolutions.backendriskassessment.services;
+
+import com.mediLaboSolutions.backendriskassessment.beans.NoteBean;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AssessmentRiskServiceImplTest {
+
+    @InjectMocks
+    AssessmentRiskServiceImpl assessmentRiskService;
+
+
+    @Test
+    void getResult() {
+    }
+}
