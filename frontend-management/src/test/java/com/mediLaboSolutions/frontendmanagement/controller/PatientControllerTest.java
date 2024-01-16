73d376a03d9132fec76b8ae1ab9eba7e8a40ea80
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-16 16:13:20 +0100

    S3-FRONTEND : correction on PatientControllerTest

--- frontend-management/src/test/java/com/mediLaboSolutions/frontendmanagement/controller/PatientControllerTest.java
+++ frontend-management/src/test/java/com/mediLaboSolutions/frontendmanagement/controller/PatientControllerTest.java
@@ -1,97 +1,87 @@
 package com.mediLaboSolutions.frontendmanagement.controller;
 
+import com.mediLaboSolutions.frontendmanagement.beans.NewPatientBean;
+import com.mediLaboSolutions.frontendmanagement.beans.PatientBean;
 import com.mediLaboSolutions.frontendmanagement.proxies.MSGateWay;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class PatientControllerTest {
 
     @Mock
-    MSGateWay msGateWay; //Todo frank
+    Model model;
+
+    @Mock
+    MSGateWay msGateWay;
 
     @InjectMocks
     PatientController patientController;
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(patientController).build();
-    }
 
     @Test
     void patientListPage() throws Exception {
         //Given
 
-        //When we initiate the request
-        mockMvc.perform(get("/patients"))
 
-                //Then we verify is all works correctly
-                .andExpect(status().isOk())
-                .andExpect(view().name("patientList"));
+        //When
+        String response = patientController.patientList(model);
+
+        //Then
+        assertEquals("patientList", response);
+
 
     }
 
     @Test
     void patientInfosPage() throws Exception {
         //Given
+        Integer id = 1;
 
-        //When we initiate the request
-        mockMvc.perform(get("/patients/1"))
+        //When
+        String response = patientController.patientInfos(id, model);
 
-                //Then we verify is all works correctly
-                .andExpect(status().isOk())
-                .andExpect(view().name("patient-details"));
+        //Then
+        assertEquals("patient-details", response);
+
     }
 
     @Test
     void newPatientPage() throws Exception {
         //Given
 
-        //When we initiate the request
-        mockMvc.perform(get("/patients/create"))
+        //When
+        String response = patientController.newPatient(model);
 
-                //Then we verify is all works correctly
-                .andExpect(status().isOk())
-                .andExpect(view().name("create-patient"));
+        //Then
+        assertEquals("create-patient", response);
     }
 
     @Test
     void createNewPatient() throws Exception {
         //Given
 
-        //When we initiate the request
-        mockMvc.perform(post("/patients/create"))
+        //When
+        String response = patientController.createNewPatient(new NewPatientBean(), model);
 
-                //Then we verify is all works correctly
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(view().name("patientList"));
+        //Then
+        assertEquals("redirect:/patients", response);
     }
 
     @Test
     void editPatient() throws Exception {
         //Given
+        Integer id = 1;
 
-        //When we initiate the request
-        mockMvc.perform(post("/patients/1"))
+        //When
+        String response = patientController.editPatient(id, new PatientBean(), model);
 
-                //Then we verify is all works correctly
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(view().name("patient-details"));
+        //Then
+        assertEquals("redirect:/patients/{id}", response);
     }
 }
