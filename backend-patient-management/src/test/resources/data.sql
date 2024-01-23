354081c22149f7a38080664c9df3efea995153e0
Giovanni Darves-Bornoz <gio.db@hotmail.fr>
2024-01-24 00:56:25 +0100

    S3-BACKEND-PATIENT : modify structure to use mysql DB for docker and H2 DB for test

--- backend-patient-management/src/test/resources/data.sql
+++ backend-patient-management/src/test/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO patient.patient (BIRTHDATE, GENDER, ADDRESS, FIRSTNAME, LASTNAME, PHONENUMBER)
-VALUES ('1966-12-31', 'F', '1 Brookside St', 'Test', 'TestNone', '100-222-3333'),
+insert into PUBLIC.PATIENT (BIRTHDATE, GENDER, ADDRESS, FIRSTNAME, LASTNAME, PHONENUMBER)
+values ('1966-12-31', 'F', '1 Brookside St', 'Test', 'TestNone', '100-222-3333'),
        ('1945-06-24', 'M', '2 High St', 'Test', 'TestBorderline', '200-333-4444'),
        ('2004-06-18', 'M', '3 Club Road', 'Test', 'TestInDanger', '300-444-5555'),
        ('2002-06-28', 'F', '4 Valley Dr', 'Test', 'TestEarlyOnset', '400-555-6666');
